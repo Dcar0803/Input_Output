97c44c3bdd9f50b92516c58591d6abf7bf843947
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-12-06 22:38:24 -0500

    Updated: Added a BeforeEach and AfterEach to clean the file before and after each test function

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -1,11 +1,34 @@
 import static org.junit.jupiter.api.Assertions.*;
+import java.io.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 class Testing {
 
 	private static final String TEST_FILE = "testFile.txt";
 	
+	
+	  @BeforeEach
+	    void setUp() throws IOException {
+	        // Clean up before each test
+	        File file = new File(TEST_FILE);
+	        if (file.exists()) {
+	            file.delete();
+	        }
+	    }//end of setUp
+	  
+	  @AfterEach
+	    void tearDown() throws IOException {
+	        // Clean up after each test
+	        File file = new File(TEST_FILE);
+	        if (file.exists()) {
+	            file.delete();
+	        }
+	    }//end of tearDown
+	  
+	  
 	@Test
     void testWriteToFile() {
 		
